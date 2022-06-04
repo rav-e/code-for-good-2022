e68dd4aa4415c5aa2605e000cfb601fe58acd44f
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2022-06-04 18:55:02 +0530

    added mock hospital and slots

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,27 +1,54 @@
-const express = require("express");
-const router = express.Router()
+// const express = require("express");
 const Hospital = require("../models/Hospital")
+const Slot = require("../models/Slot")
 
-const getHospitalsData = async () => { 
+const sampleHospitals = [
+    {
+        title: "Laxmi Trust Hospital",
+        password: "Pwd1234"
+    }, {
+        title: "Sri Ramakrishna Hospital",
+        password: "Pwd1234",
+    }, {
+        title: "Dentronix Medisystems",
+        password: "Pwd1234"
+    }, {
+        title: "Manav Kalyan Seva Samiti",
+        password: "Pwd1234"
+    }
+]
+
+const saveMockHospitals = async () => {
+    // await Hospital.deleteMany({})
+    await Promise.all(sampleHospitals.map(async (hospital) => {
+        console.log("hosp", hospital)
+        const hospitalDoc = new Hospital(hospital)
+        console.log("hospdoc", hospitalDoc)
+        return await hospitalDoc.save()
+    }))
+}
+
+const getHospitalsData = async () => {
     const hospitals = await Hospital.find({})
-    const slots = await Slots.find({})
+    const slots = await Slot.find({})
 
     let resp = []
 
-    hospitals.forEach((hospital) => { 
-        
+    hospitals.forEach((hospital) => {
+
         const curHospitalSlots = slots.filter((slot) => slot.hospital == hospital.id)
-        
+
         resp.push({
             ...hospital,
-            slots : curHospitalSlots
+            slots: curHospitalSlots
         })
     })
-
+    console.log("hospitals data", resp)
     return resp
 }
 // router.get("/", async (req, res) => { 
 //     return await Hospital.find({});
 // })
 
-module.exports = router
+exports.getHospitalsData = getHospitalsData
+exports.saveMockHospitals = saveMockHospitals
